544fc291e011e7324296c1b64adcc69391365fa4
Dilschat <dilschat@yandex.ru>
2019-04-03 21:11:40 +0300

    test endpoints added

--- backend/api/test.py
+++ backend/api/test.py
@@ -2,27 +2,10 @@ from flask_jwt_extended import jwt_required, jwt_refresh_token_required, get_jwt
     get_raw_jwt, create_refresh_token
 from flask_restful import Resource, reqparse
 
-from app.helpers import success_response, fail_response
+from backend import helpers
 
 
-
-
-class ActualTest(Resource):
-    @jwt_required
-    def post(self):
-        pass
-
-
-    @jwt_required
-    def get(self):
-        pass
-
-    @jwt_required
-    def put(self):
-        pass
-
-
-class ArchivedTest(Resource):
+class TestList(Resource):  # /test
     @jwt_required
     def post(self):
         pass
@@ -31,22 +14,48 @@ class ArchivedTest(Resource):
     def get(self):
         pass
 
-    @jwt_required
-    def put(self):
-        pass
 
-
-class TestAnswers(Resource):
+class Test(Resource):  # test/id put get delete
     @jwt_required
-    def post(self):
+    def delete(self, test_id):
         pass
 
     @jwt_required
-    def get(self):
+    def put(self, test_id):
+        pass
+
+
+class TimerStart(Resource):  # test/id/start post
+    @jwt_required
+    def post(self, test_id):
+        pass
+
+
+class TimerCheckpoint(Resource):  # test/id/checkpoint post
+    @jwt_required
+    def post(self, test_id):
+        pass
+
+
+class TimerEnd(Resource):  # test/id/submit post
+    @jwt_required
+    def post(self, test_id):
+        pass
+
+
+class SubmissionsList(Resource):  # submissions/test_id/list get
+    @jwt_required
+    def get(self, test_id):
+        pass
+
+
+class Submission(Resource):  # submissions/submission_id get post
+    @jwt_required
+    def post(self, submission_id):
         pass
 
     @jwt_required
-    def put(self):
+    def get(self, submission_id):
         pass
 
 
