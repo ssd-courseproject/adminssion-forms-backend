fb5b8b0885f3e8b9ca77ec1da86323f7f2a9877e
Dilschat <dilschat@yandex.ru>
2019-04-03 21:18:05 +0300

    utils created

--- backend/api/test.py
+++ backend/api/test.py
@@ -2,8 +2,8 @@ from flask_jwt_extended import jwt_required, jwt_refresh_token_required, get_jwt
     get_raw_jwt, create_refresh_token
 from flask_restful import Resource, reqparse
 
-from backend import helpers
-
+from backend.helpers import fail_response,success_response
+from backend.core import models
 
 class TestList(Resource):  # /test
     @jwt_required
@@ -23,6 +23,12 @@ class Test(Resource):  # test/id put get delete
     @jwt_required
     def put(self, test_id):
         pass
+
+    @jwt_required
+    def get(self, test_id):
+        if not Utils.is_test_exist(test_id):
+            return fail_response(msg="Test is not exist", code=404)
+        models.
 
 
 class TimerStart(Resource):  # test/id/start post
@@ -59,3 +65,10 @@ class Submission(Resource):  # submissions/submission_id get post
         pass
 
 
+class Utils:
+    @staticmethod
+    def is_test_exist(test_id):
+        if test_id == 1:
+            return True
+        else:
+            return False
